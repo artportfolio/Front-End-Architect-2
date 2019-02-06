8232287881a85b9182ed352492b270e75f59ba52
BrannanC <brannanconrad@gmail.com>
2019-02-06 11:39:56 -0600

    Displays random photo if imageUrl doesn't contain http

--- build-week-artful/src/components/Items/Item.js
+++ build-week-artful/src/components/Items/Item.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Item = props => {
+    const words = ['nature', 'water', 'fire', 'air', 'mountain', 'animal', 'urban', 'landscape', 'canyon', 'flying'];
+    const num = Math.floor(Math.random() * 10);
     return (
         <div className="Item">
             <p className="username">{props.user.username}</p>
-            <img src={props.photo.imageUrl} />
+            <img src={props.photo.imageUrl.includes('http') ? props.photo.imageUrl : `https://source.unsplash.com/600x800/?${words[num]}`} alt={props.photo.title} />
         </div>
     );
 }
